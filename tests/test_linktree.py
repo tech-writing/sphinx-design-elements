756cedd012fcbf3c892d2fd0ab65b59b3dab47a2
Andreas Motl <andreas.motl@panodata.org>
2023-07-27 23:55:35 +0200

    Add "link tree" element, using directive `linktree`

---
+++ tests/test_linktree.py
@@ -0,0 +1,27 @@
+from sphinx_design_elements.navigation import demo_tree
+
+
+def test_linktree_demo_tree(sphinx_html_builder):
+    builder = sphinx_html_builder()
+    builder.init()
+    builder.init_templates()
+    builder.init_highlighter()
+
+    tree = demo_tree(builder=builder, context={})
+    html = tree.render()
+
+    assert '<p class="caption" role="heading"><span class="caption-text">Project-local page links</span></p>' in html
+    # FIXME: Apparently, references to documents can not be resolved yet, in testing mode.
+    assert '<li class="toctree-l2"><a class="reference internal" href="#">None</a></li>' in html
+
+    assert '<p class="caption" role="heading"><span class="caption-text">Intersphinx links</span></p>' in html
+    assert (
+        '<li class="toctree-l2"><a class="reference internal" href="https://sphinx-design.readthedocs.io/en/latest/index.html">sphinx-design</a></li>'
+        in html
+    )
+
+    assert '<p class="caption" role="heading"><span class="caption-text">URL links</span></p>' in html
+    assert (
+        '<li class="toctree-l2"><a class="reference internal" href="https://example.com">https://example.com</a></li>'
+        in html
+    )
