40326b7deb3a2472835c6d992d08bb47da782549
Andreas Motl <andreas.motl@panodata.org>
2025-12-13 04:36:07 +0100

    Project: Populate `__version__` variable properly

--- sphinx_design_elements/__init__.py
+++ sphinx_design_elements/__init__.py
@@ -1,8 +1,15 @@
 """A sphinx extension for designing beautiful, view size responsive web components."""
 
+from importlib import metadata
 from typing import TYPE_CHECKING
 
-__version__ = "0.0.0"
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:  # pragma: no cover
+    # Case where package metadata is not available.
+    __version__ = "0.0.0"
+del metadata  # optional, avoids polluting the results of dir(__package__)
+
 
 if TYPE_CHECKING:
     from sphinx.application import Sphinx
