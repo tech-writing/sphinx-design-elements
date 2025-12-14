23f8faa84648e5e4f3e5cb9f26ff0cf3b85a73c0
Andreas Motl <andreas.motl@panodata.org>
2025-12-14 03:02:57 +0100

    LinkTree: Fix CI (increased code coverage)

--- tests/test_linktree.py
+++ tests/test_linktree.py
@@ -1,7 +1,21 @@
+from typing import Callable
+
 import docutils
 from verlib2 import Version
 
 from sphinx_design_elements.navigation import demo_tree
+from tests.conftest import SphinxBuilder
+
+
+def test_directive(sphinx_builder: Callable[..., SphinxBuilder]):
+    """Quickly test `linktree` directive."""
+    builder = sphinx_builder()
+    content = "# Heading" + "\n\n\n" + ":::{linktree}\n:::\n\n"
+    builder.src_path.joinpath("index.md").write_text(content, encoding="utf8")
+    builder.build(assert_pass=False)
+    assert "Producing link tree for: index" in builder.status
+    assert "build succeeded, 1 warning" in builder.status
+    assert "WARNING: toctree is empty" in builder.warnings
 
 
 def test_linktree_demo_tree(sphinx_html_builder):
